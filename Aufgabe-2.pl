2439afc3c8ce50274d227b6a7561f501fac9a86c
Felix2398 <104097884+Felix2398@users.noreply.github.com>
2022-07-09 21:55:43 +0200

    Add files via upload

---
+++ Aufgabe-2.pl
@@ -0,0 +1,27 @@
+% Aufgabe 2.a)
+sport(anja,tennis).
+sport(bernd, tennis).
+sport(bernd, golf).
+sport(claudia, billiard).
+sport(claudia, tennis).
+sport(daniel, billiard).
+sport(elke, golf).
+sport(frank, tennis).
+sport(frank, billiard).
+
+time(anja, montag).
+time(anja, dienstag).
+time(bernd, dienstag).
+time(claudia, donnerstag).
+time(daniel, mittwoch).
+time(elke, freitag).
+time(elke, samstag).
+time(frank, donnerstag).
+
+% Aufgabe 2.b)
+% ?-time(X, freitag),time(X, samstag)
+
+% Aufgabe 2.c)
+% ?-sport(X,Z),sport(Y,Z),time(X,W),time(Y,W),X\=Y
+
+
